5488c2fc3187b61fcbc233c48d719a58f3b7abaa
zwycooky <zwycooky@163.com>
2024-02-25 18:01:03 +0800

    Update single.contig.pl

--- 00_Softwares/Rephasing-script/single.contig.pl
+++ 00_Softwares/Rephasing-script/single.contig.pl
@@ -3,13 +3,15 @@
 use strict;
 use Cwd;
 use Cwd 'abs_path';
+use Parallel::ForkManager;
 
 my $dir = @ARGV[0];
 my $dir2 = @ARGV[1];
 my $dir3 = @ARGV[2];
-my $thread = @ARGV[3];
-my $Usage = "\n\t$0 <input vcf dir> <script folder> <output folder> <thread>\n";
-die $Usage unless (@ARGV == 4);
+my $hapi_thread = @ARGV[3];
+my $thread = @ARGV[4];
+my $Usage = "\n\t$0 <input vcf dir> <script folder> <output folder> <hapi thread> <thread>\n";
+die $Usage unless (@ARGV == 5);
 
 $dir = abs_path($dir);
 
@@ -20,6 +22,9 @@ my $dir3 = abs_path($dir3);
 
 mkdir $dir2;
 chdir $dir2;
+
+## ForkManager setup ##
+my $pm = Parallel::ForkManager->new($thread);
 
 my @file = glob "$dir/*.vcf";
 
@@ -32,17 +37,20 @@ foreach (@file) {
                 my $vcf_list = join(",",@tmp);
                 $jobs ++;
                 $counter = 0;
-                &phasing_sge($vcf_list,$jobs);
+				$pm->start and next;
+				&phasing_sge($vcf_list,$jobs);
+				$pm->finish;
                 @tmp = ();
         }
 }
+$pm->wait_all_children();
 
 
 sub phasing_sge {
         my ($vcf_list,$job_id) = @_;
         my $sge = "
 
-$phasing_script $vcf_list $dir3 $thread
+$phasing_script $vcf_list $dir3 $hapi_thread
 ";
         open SGE,'>',"$job_id.sh";
         print SGE "$sge";
