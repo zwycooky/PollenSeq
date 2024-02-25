9813427568f58d4a757ce539a1db6e93490255a6
zwycooky <zwycooky@163.com>
2024-02-25 13:24:03 +0800

    Create batch_mapit.pl

---
+++ 00_Softwares/Rephasing-script/batch_mapit.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+use strict;
+use Cwd 'abs_path';
+use Parallel::ForkManager;
+
+my ($inputdir,$outputdir,$ref_genome,$mapping_cpu, $cpu) = @ARGV[0,1,2,3,4];
+my $Usage = "\n\t$0 <input fastq dir> <out bam dir> <genome> <mapping threads> <threads>
+\n";
+die $Usage unless (@ARGV == 5);
+
+chomp(my $pwd = `pwd`);
+
+if (!-e $outputdir) {
+	mkdir $outputdir;
+}
+
+$inputdir = abs_path($inputdir);
+$outputdir = abs_path($outputdir);
+my $mapping_script = "02_mapit.pl";
+
+## ForkManager setup ##
+my $pm = Parallel::ForkManager->new($cpu);
+
+
+chomp(my @fq = `find $inputdir -name "*.gz"`);
+my $count = 0;
+my ($fq1,$fq2);
+foreach (sort @fq) {
+	$count ++;
+	if ($count < 2) {
+		$fq1 = abs_path($_);
+	}else{
+		$fq2 = abs_path($_);
+		my $id = (split /\./,(split /\//,$fq2)[-1])[0];
+		$count = 0;
+		$pm->start and next;
+        	&mapping($fq1,$fq2,$id,$outputdir,$ref_genome,$mapping_cpu);
+        	$pm->finish;
+	}
+}
+$pm->wait_all_children();
+
+sub mapping {
+	my ($fq1,$fq2,$id,$outdir,$genome,$mapping_cpu) = @_;
+	!system "$mapping_script -1 $fq1 -2 $fq2 -f $genome -b $genome -p $mapping_cpu -o $outdir/$id" or die "ERROR with fastp:$!";
+}
+
