8cd6f289b3a2a9d22c07f32460286bc2af6785b8
zwycooky <zwycooky@163.com>
2024-11-04 13:58:06 +0800

    Update merge_split_vcf.pl

--- 00_Softwares/Rephasing-script/merge_split_vcf.pl
+++ 00_Softwares/Rephasing-script/merge_split_vcf.pl
@@ -8,7 +8,7 @@ my $Usage = "\n\t$0 <Raw_split_vcf.list> <output file>
 die $Usage unless (@ARGV == 2);
 
 
-my (%ex);
+my (%ex,$prechr,$prepos);
 open OUT,'>',"$output" or die;
 my $count = 0;
 open IN,'<',"$input" or die;
@@ -24,19 +24,33 @@ while (<IN>) {
 				my ($chr,$pos) = (split)[0,1];
 				my $key = "$chr\t$pos";
 				$ex{$key} = 1;
+				$prechr = $chr;
+                		$prepos = $pos;
 			}
 		}
 		close VCF;
 		$count = 1;
 	}else{
 		open VCF,'<',"$path" or die "Cannot open $path:$!";
-        while (<VCF>) {
-            chomp;
+        	while (<VCF>) {
+            		chomp;
 			if (/\A#/) { next };
 			my ($chr,$pos) = (split)[0,1];
-            my $key = "$chr\t$pos";
+            		my $key = "$chr\t$pos";
+			my ($chr,$pos,$ref,$alt) = (split)[0,1,3,4];
+        		if ($prechr eq $chr){
+                		if ($pos < $prepos) {
+					#print "$chr\t$pos\n";
+					next;
+                		}else{
+                        		$prepos = $pos;
+                		}
+			}elsif ($prechr ne $chr){
+                		$prechr = $chr;
+                		$prepos = $pos;
+        		}
 			if (!exists $ex{$key}) {
-                print OUT "$_\n";
+				print OUT "$_\n";
 				$ex{$key} = 1;
 			}
 		}
